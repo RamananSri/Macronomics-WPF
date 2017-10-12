3e9431f6a39e3f2dd3a545cdc03ae77e815a745c
Ramanan <1058515@ucn.dk>
2017-10-12 17:43:56 +0200

    MainWindow page loading + LoginPage layout

--- Macronomics/MainWindow.xaml.cs
+++ Macronomics/MainWindow.xaml.cs
@@ -10,19 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Macronomics
 {
     /// <summary>
-    /// Interaction logic for LoginPage.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class LoginPage : Page
+    public partial class MainWindow : Window
     {
-        public LoginPage()
+        public MainWindow()
         {
             InitializeComponent();
+            this.Content = new LoginPage(this);
         }
     }
 }
---
+++ Macronomics/Pages/LoginPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Macronomics
+{
+    public partial class LoginPage : Page
+    {
+        private readonly Window _window;
+
+        public LoginPage(Window mainWindow)
+        {
+            _window = mainWindow;
+            InitializeComponent();
+        }
+
+        private void LoginBtnClicked(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTxt.Text;
+            string password = PasswordTxt.Password;
+            if (!String.IsNullOrEmpty(UsernameTxt.Text) && !String.IsNullOrEmpty(PasswordTxt.Password))
+            {
+                Login(username, password);
+            }
+        }
+
+        private void Login(string username, string password{
+
+
+        }
+    }
+}
