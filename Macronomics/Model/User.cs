40a38e33b83c873b2fa2025f2bc7aee5f9ac4e4a
Daniel070793 <daniel070793@gmail.com>
2017-10-12 16:00:45 +0200

    added small pages

---
+++ Macronomics/Model/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Macronomics.Model
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+
+        private Gender gender;
+
+        public Gender GetGender()
+        {
+            return gender;
+        }
+
+        public void SetMyProperty(Gender value)
+        {
+            gender = value;
+        }
+    }
+    public enum Gender
+    {
+        Male, Female
+    }
+}
